b85d205f1f6d99fcc01ecf9dc123b8f30f69cfda
Zainal Arifin <128154324+elzains@users.noreply.github.com>
2023-04-06 05:14:06 +0700

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -19,7 +19,7 @@ let config = {
       "icon_classes": "fas fa-link",
     },
     {
-      "Title": "DESKRIPSI++",
+      "Title": "DESKRIPSI++ (MAINTENANCE)",
       "URL": "../../maintenance.html",
       "icon_classes": "fas fa-link",
     },
